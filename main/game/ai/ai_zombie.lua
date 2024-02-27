a5129eb3f620dd4cb619f16d3be1869f602524b6
decadent42 <decadent42@yandex.ru>
2024-02-27 16:05:51 +0300

    Сделал обзор у зомбиков на врагов. Сделал обзор у зомбиков на врагов после атаки.

--- main/game/ai/ai_zombie.lua
+++ main/game/ai/ai_zombie.lua
@@ -41,6 +41,38 @@ function M.condition_to_horde(self, url)
 	self.check_attak = M.check_distantion_attack(self, self.target)
 end
 
+-- Обзор окружения
+function M.view(self, handle_enemy)
+	local function view(self)
+		local visible_items = ai_vision.get_visible(self, self.visible_object_id, self.distantion_visible)
+
+		
+		if visible_items and #visible_items > 0 and handle_enemy then
+			pprint("handle_enemy")
+			if handle_enemy(self, visible_items) and self.view then
+				self.view.stop(self)
+			end
+		end
+	end
+
+	local function stop(self)
+		if self.timer_view then
+			timer.cancel(self.timer_view)
+			self.timer_view = nil
+		end
+	end
+
+	stop(self)
+
+	-- Смотрим вокруг
+	view(self)
+	self.timer_view = timer.delay(self.time_view, true, function (self)
+		view(self)
+	end)
+
+	return {stop = stop}
+end
+
 -- Дистанция для атаки
 function M.check_distantion_attack(self, url)
 	local function attack(self)
