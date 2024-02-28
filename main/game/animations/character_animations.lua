6160ad0c29393ce8dc318edf9748146088405dbc
decadent42 <decadent42@yandex.ru>
2024-02-28 17:01:49 +0300

    Сделал партиклы для крови от дамага. Сделал анимацию подскакивания при дамаге. Сделал анимацию покраснения при дамаге. Сделал анимацию крови при дамаге.

--- main/game/animations/character_animations.lua
+++ main/game/animations/character_animations.lua
@@ -1,7 +1,7 @@
 -- Анимации
 local M = {}
 
--- Анимации персонажа
+-- Анимации передвижения персонажа
 function M.play(self, animation_id)
 	if animation_id == "move" or animation_id == "idle" then
 		if animation_id == "move" then
@@ -20,6 +20,41 @@ function M.play(self, animation_id)
 		sprite.play_flipbook("#body", "zombie_"..self.skin_id.."_"..self.human_id.."_attack", function (self)
 			self.last_animation = nil
 			self.hflip_stop = nil
+		end)
+	end
+end
+
+-- Анимация дамага
+function M.damage(self, from_object_damage)
+	-- Анимация дамага
+	if not self.particle then
+		local duration = 0.15
+		local position = go.get_position()
+		local dir = go.get_position(from_object_damage) - position
+		local particle_name
+
+		-- Отпрыгивание
+		if dir.x < 0 then
+			position.x = position.x + 3
+			position.y = position.y + 3
+			particle_name = "#blood_right"
+		else
+			position.x = position.x - 3
+			position.y = position.y - 3
+			particle_name = "#blood_left"
+		end
+		go.animate(".", "position.x", go.PLAYBACK_ONCE_FORWARD, position.x, go.EASING_LINEAR, duration, 0)
+		go.animate(".", "position.y", go.PLAYBACK_ONCE_PINGPONG, position.y, go.EASING_LINEAR, duration, 0)
+
+		go.set("#body", "tint", vmath.vector4(1, 0.6, 0.6, 1)) -- <1>
+
+		if self.damage_blood then
+			particlefx.play(particle_name)
+		end
+		self.particle = true
+		timer.delay(duration, false, function (self)
+			go.set("#body", "tint", vmath.vector4(1, 1, 1, 1)) -- <1>
+			self.particle = nil
 		end)
 	end
 end
--- main/game/characters/zombie/zombie.go
+++ main/game/characters/zombie/zombie.go
@@ -23,6 +23,36 @@ components {
     type: PROPERTY_TYPE_NUMBER
   }
 }
+components {
+  id: "blood_right"
+  component: "/main/game/effects/blood.particlefx"
+  position {
+    x: 5.0
+    y: 6.0
+    z: -1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: -0.70710677
+    w: 0.70710677
+  }
+}
+components {
+  id: "blood_left"
+  component: "/main/game/effects/blood.particlefx"
+  position {
+    x: -5.0
+    y: 6.0
+    z: -1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
 embedded_components {
   id: "body"
   type: "sprite"
