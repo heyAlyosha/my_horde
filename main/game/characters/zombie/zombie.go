dd7aa890b75e305444ad30bbbe337e46bf780557
decadent42 <decadent42@yandex.ru>
2024-02-29 10:36:55 +0300

    Добавил тени для зомбиков. Добавил тени для игрока., Добавил тени для предметов.

--- main/game/characters/zombie/zombie.go
+++ main/game/characters/zombie/zombie.go
@@ -198,3 +198,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "shadow"
+  type: "sprite"
+  data: "default_animation: \"shadow\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/atlases/first_level.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: -1.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
