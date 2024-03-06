d7d1197305d6befe24828d9cf9b322983b302f12
decadent42 <decadent42@yandex.ru>
2024-03-06 13:28:30 +0300

    Поправил следование зомбиков в орду за игроком.

--- main/game/ai/ai_zombie.lua
+++ main/game/ai/ai_zombie.lua
@@ -88,19 +88,21 @@ function M.behavior(self)
 					self.condition_attack = nil
 
 					if self.parent and go_controller.url_to_key(self.parent) ~= go_controller.url_to_key(msg.url()) and go_controller.is_object(self.parent) then
-						self.target_vector, self.target_add_horde = M.get_horde_position(self)
+						self.target_add_horde = go.get_position(self.parent)
 						local function handle_success(self)
 							--Добежал до пункта
-							self.target_vector, self.target_add_horde = M.get_horde_position(self)
+							-- Цель до орды
+							self.target_add_horde = go.get_position(self.parent)
 
 							-- Смотрим дистацию до игрока
-							local dist = vmath.length(self.target_add_horde - go.get_position())
+							local position_from = go.get_position()
+							local dist = vmath.length(self.target_add_horde - position_from)
 							if dist <= 10 then
 								-- Добежал
-								print("handle_success", dist, self.target_add_horde)
 								msg.post(self.parent, "add_horde", {
 									skin_id = self.skin_id,
 									human_id = self.human_id,
+									position_from = position_from
 								})
 								go.delete()
 							else
@@ -108,6 +110,7 @@ function M.behavior(self)
 								self.condition_to_horde = nil
 								self.target = nil
 								self.condition_attack = nil
+								ai_move.stop(self)
 
 								ai_zombie.behavior(self)
 							end
@@ -115,20 +118,25 @@ function M.behavior(self)
 
 						local function handle_error(self, error_code)
 							-- Не может найти путь к месту в орде
-							self.target_vector, self.target_add_horde = M.get_horde_position(self)
-							local duration = vmath.length(self.target_add_horde - go.get_position()) / self.speed
-							print("handle_error")
+							self.target_add_horde = go.get_position(self.parent)
+							local position_from = go.get_position()
+							local duration = vmath.length(self.target_add_horde - position_from) / self.speed
+							
 							go.animate(go.get_id(), "position", go.PLAYBACK_ONCE_FORWARD, self.target_add_horde, go.EASING_LINEAR, duration, 0, function (self)
 								msg.post(self.parent, "add_horde", {
 									skin_id = self.skin_id,
 									human_id = self.human_id,
+									position_from = position_from
 								})
 								go.delete()
 							end)
 						end
 
 						-- Пересчитываем каждый тайтл пути
-						local function handle_item_move(self) end
+						local handle_item_move = function (self)
+							
+							handle_success(self)
+						end
 						ai_move.move_to_position(self, self.target_add_horde, handle_success, handle_error, handle_no_object_target, handle_item_move)
 
 					else
