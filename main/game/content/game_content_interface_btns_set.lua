19af78eda2f3a4353f96b25fa902cfbfd1b9d3e7
decadent42 <decadent42@yandex.ru>
2024-03-12 13:52:11 +0300

    Сделал запуск победной анимации с грозой через сообщения.

--- main/game/content/game_content_interface_btns_set.lua
+++
@@ -1,9 +0,0 @@
--- Сеты для кнопок справа вверху
-local M = {
-	default = {"volume", "settings","shop",},
-	game_play = {"volume", "pause"},
-	game_pause = { "volume", "settings"},
-	testing = { "animate_rating", "settings"},
-}
-
-return M
