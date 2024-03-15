40d4a579671de6f7d86b9b53ae976bbddbe9d711
decadent42 <decadent42@yandex.ru>
2024-03-15 18:08:01 +0300

    Сделал отправку звёздочек в интерфейс. Сделал анимацию заполнения звёздочек в интерфейсе. Сделал настройку звёздочек при загрузке карты.

--- main/gui/interface/modules/interface_stars.lua
+++ main/gui/interface/modules/interface_stars.lua
@@ -13,52 +13,30 @@ local lang_core = require "main.lang.lang_core"
 function M.visible(self, visible)
 	self.visible_stars = visible
 	gui_loyouts.set_enabled(self, self.nodes.stars_wrap, visible)
+
+	if not visible then
+		--Сбрасываем заполненные звёздочки
+		gui.animate(self.nodes.wrap_stars_active, "size.x", 0, gui.EASING_OUTCUBIC, 0.001)
+	end
 end
 
 -- Добавил или убавили звёздочки
 function M.set_star(self, stars, unwrap)
 	timer_linear.add(self, "stars", 0, function (self)
-		if self.set_star_timer then
-			self.set_star_timer.stop(self)
-			self.set_star_timer = nil
-		end
-		msg.post("main:/sound", "play", {sound_id = "modal_top_2_2"})
-		self.set_star_timer = gui_animate.areol(self, "aoreol_template", speed_to_second, "loop", function_end, scale)
-		gui.animate(self.nodes.stars_wrap, "scale", vmath.vector3(1.1), gui.EASING_LINEAR, 0.25)
+		--msg.post("main:/sound", "play", {sound_id = "modal_top_2_2"})
+		gui.animate(self.nodes.stars_wrap, "scale", vmath.vector3(0.8), gui.EASING_LINEAR, 0.1)
 	end)
 	
 	-- Анимация звёздочек
-	timer_linear.add(self, "stars", 1, function (self)
-		for star_i = 1, 3, 1 do
-			local node_star = self.nodes["star_"..star_i]
-			local sprite = gui.get_flipbook(node_star)
-			local get_active = sprite == hash("star_active")
-			local set_active = star_i <= stars
-
-			if set_active ~= get_active then
-				gui_animate.set_star(self, node_star, duration, delay, function_end_animation, set_active)
-				gui_animate.set_star(self, self.nodes["list_item_"..star_i].icon, duration, delay, function_end_animation, set_active)
-			end
-		end
+	timer_linear.add(self, "stars", 0.1, function (self)
+		local size_wrap_start_active = gui.get_size(self.nodes.wrap_stars_bg).x / 3 * stars
+		gui.animate(self.nodes.wrap_stars_active, "size.x", size_wrap_start_active, gui.EASING_OUTCUBIC, 0.25)
 	end)
 
-	timer_linear.add(self, "stars", 0.5, function (self)
-		self.set_star_timer.stop(self)
-		self.set_star_timer = nil
-		gui.animate(self.nodes.stars_wrap, "scale", vmath.vector3(1), gui.EASING_LINEAR, 0.25)
+	-- Анимация звёздочек
+	timer_linear.add(self, "stars", 0.25, function (self)
+		gui.animate(self.nodes.stars_wrap, "scale", vmath.vector3(0.75), gui.EASING_LINEAR, 0.1)
 	end)
-
-	--
-	if unwrap and stars < 3 then
-		
-		timer_linear.add(self, "stars", 0.5, function (self)
-			M.unwrap(self, true, true)
-		end)
-		timer_linear.add(self, "stars", 5, function (self)
-			M.unwrap(self, false, true)
-		end)
-		
-	end
 end
 
 return M
