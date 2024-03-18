cb540770e6949e587feb2fe2eaf54aeb2b33fcea
decadent42 <decadent42@yandex.ru>
2024-03-18 13:17:37 +0300

    Сделал анимацию появления блоков.

--- main/gui/modals/modal_result/modules/modal_result_render.lua
+++ main/gui/modals/modal_result/modules/modal_result_render.lua
@@ -22,10 +22,12 @@ local lang_core = require "main.lang.lang_core"
 local live_update_atlas = require "main.game.live_update.atlas.live_update_atlas"
 
 function M.visible(self, data)
-	-- Показываем или скрываем ненужные блоки
-	gui_loyouts.set_enabled(self, self.nodes.wrap_btns_win, self.data.type_result == "win")
-	gui_loyouts.set_enabled(self, self.nodes.wrap_btns_fail, self.data.type_result == "fail")
-	gui_loyouts.set_enabled(self, self.nodes.wrap_trophy, self.data.type_result == "win")
+	local nodes_wrap
+	-- Скрываем блоки
+	gui_loyouts.set_enabled(self, self.nodes.title_wrap, false)
+	gui_loyouts.set_enabled(self, self.nodes.wrap_btns_win, false)
+	gui_loyouts.set_enabled(self, self.nodes.wrap_btns_fail, false)
+	gui_loyouts.set_enabled(self, self.nodes.wrap_trophy, false)
 
 	-- Отрисовываем Поражение или победу
 	if self.data.type_result == "win" then
@@ -40,6 +42,12 @@ function M.visible(self, data)
 			{id = "continue", type = "btn", section = "body",  icon = "btn_interface_"},
 			{id = "shop", type = "btn", section = "body", icon = "btn_interface_"},
 		}
+
+		nodes_wrap = {
+			self.nodes.title_wrap,
+			self.nodes.wrap_trophy,
+			self.nodes.wrap_btns_win,
+		}
 	else
 		-- 
 		gui_loyouts.set_color(self, self.nodes.title, color.red)
@@ -51,9 +59,14 @@ function M.visible(self, data)
 			{id = "refresh", type = "btn", section = "body", icon = "btn_interface_"},
 			{id = "shop", type = "btn", section = "body", icon = "btn_interface_"},
 		}
+
+		nodes_wrap = {
+			self.nodes.title_wrap,
+			self.nodes.wrap_btns_fail,
+		}
 	end
 
-	--подставляем ноды в кнопки
+	--Подставляем ноды в кнопки
 	for i, btn in ipairs(self.btns) do
 		self.btns[i].node = gui.get_node("btn_"..self.data.type_result.."_"..i.."_template/btn")
 		if btn.id ~= "shop" then
@@ -61,7 +74,26 @@ function M.visible(self, data)
 		end
 	end
 
-	-- АНИМАЦИЯ ПОЯВЛЕНИЯ ЭЛЕМЕНТОВ ПОБЕДЫ
+	-- АНИМАЦИЯ ПОЯВЛЕНИЯ ЭЛЕМЕНТОВ
+	local delay = 0
+	local duration = 0.2
+
+	-- Блоки с контентом
+	for i, node in ipairs(nodes_wrap) do
+		timer_linear.add(self, "start", delay, function (self)
+			gui_animate.show_elem_popping(self, node , nil)
+		end)
+
+		delay = delay + duration
+	end
+
+	-- Награда
+	if self.data.type_result == "win" then
+		timer_linear.add(self, "start", 0, function (self)
+			
+		end)
+	end
+	
 	--[[
 	modal_result_single_animate_win.start(self, data, nil, function (self)
 		--modal_result_single_btns.render_login_btn(self)
