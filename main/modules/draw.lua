ce9f1f27d4941176076b88413239b507c5f987c3
decadent42 <decadent42@yandex.ru>
2024-03-18 17:58:27 +0300

    ВЫнес ортографическую камеру отдельный игровой объект.

--- main/modules/draw.lua
+++
@@ -1,8 +0,0 @@
--- Отрисовка для дебага
-local M = {}
-
-function M.line(from, to)
-	msg.post("@render:", "draw_line", { start_point = from, end_point = to, color = vmath.vector4(1,0,0,1) })
-end
-
-return M
