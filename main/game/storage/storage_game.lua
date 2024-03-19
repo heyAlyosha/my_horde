bbe0375791353c65b748554dc3241fd03bf86122
decadent42 <decadent42@yandex.ru>
2024-03-19 13:11:02 +0300

    Сделал запись прогресса при прохождении уровня. Сделал высчитывание звёзд при прохождении уровня.

--- main/game/storage/storage_game.lua
+++ main/game/storage/storage_game.lua
@@ -5,32 +5,9 @@ local color = require("color-lib.color")
 
 -- Хранилище для игры
 M.game = {
-	study = false, -- Обучение
-	-- Сообщение для старта (для перезапуска миссии)
-	message_start = {},
-	-- Данные для игрового раунда
-	round = {
-		type = "single/family",
-		level = {},
-		category = {},
-		quest = "Текст вопроса",
-		quest_type = 'text/image/sound/video',
-		word = "слово",
-		disable_symbols = {}, -- Заблокированные буквы для ответа
-		tablo = {}, -- Состояние табло,
-		is_stars = nil
-	},
-	-- Результаты игрового раунда
-	result = {
-		xp = 1200, -- Опыт
-		score = 1200, -- Для покупок подарков в магазине
-		prizes = {},
-		player_win_id = "player",
-		stars = 3
-	},
-	players = {}
+	level_id = nil,
+	company_id = nil
 }
-
 
 -- Хранилище для завёзд за миссию
 M.stars = {}
