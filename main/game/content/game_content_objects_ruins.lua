b1f3f9b9d0790059b172637255e7735fa3f3bc30
decadent42 <decadent42@yandex.ru>
2024-04-04 18:36:10 +0300

    Сделал запись характеристик для разрушаемых объектов из таблицы.

---
+++ main/game/content/game_content_objects_ruins.lua
@@ -0,0 +1,38 @@
+-- Разрушаемые объекты
+local storage_player = require "main.storage.storage_player"
+
+local M = {}
+
+M.catalog_keys = {}
+M.catalog = {}
+
+function M.init(self)
+	local is_replace_placeholder = false
+	M.catalog_keys = game_content_functions.load_content(self, "objects_ruins", group_columns, function (self, row_id, item)
+
+	end, is_replace_placeholder)
+
+	for k, v in pairs(M.catalog_keys) do
+		M.catalog_keys[hash(k)] = v
+	end
+	pprint(M.catalog_keys)
+end
+
+-- Получить характеристику под тип
+function M.get_type(self, type_id)
+	return M.catalog_keys[type_id]
+end
+
+-- Получить характеристику под тип
+function M.set_characteristic(self, id)
+	local characteristics = M.get_type(self, id_characteristic or self.type_object_ruin)
+	pprint("set_characteristic", characteristics)
+
+	if characteristics then
+		for k, v in pairs(characteristics) do
+			self[k] = v
+		end
+	end
+end
+
+return M
