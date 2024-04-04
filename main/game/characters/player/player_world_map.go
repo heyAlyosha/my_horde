d1780ddabd8082b988ecf0575a1a4b5a5ee9ad11
decadent42 <decadent42@yandex.ru>
2024-04-04 10:32:44 +0300

    Сделал гуи для анимации поднимания предметов с вождя.

--- main/game/characters/player/player_world_map.go
+++ main/game/characters/player/player_world_map.go
@@ -63,6 +63,21 @@ components {
     w: 0.70710677
   }
 }
+components {
+  id: "add_trofey"
+  component: "/main/gui/game_gui/add_trofey/add_trofey.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collision_physic"
   type: "collisionobject"
---
+++ main/gui/game_gui/add_trofey/add_trofey.go
@@ -0,0 +1,15 @@
+components {
+  id: "add_trofey"
+  component: "/main/gui/game_gui/add_trofey/add_trofey.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
