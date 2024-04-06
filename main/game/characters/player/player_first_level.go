6699830e1033ae74c41cc61dc12a4f76f311a457
decadent42 <decadent42@yandex.ru>
2024-04-06 10:27:35 +0300

    Сделал таблицу с характеристиками игрока. Сделал таблицу с характеристиками орды. Добавил переводы для характеристик. Удалил лищние переводы для характеристик.

--- main/game/characters/player/player_first_level.go
+++ main/game/characters/player/player_first_level.go
@@ -53,6 +53,21 @@ components {
     w: 0.70710677
   }
 }
+components {
+  id: "inventary_wrap"
+  component: "/main/gui/inventary_wrap/inventary_wrap.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "body"
   type: "sprite"
